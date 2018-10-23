f7dc06899c256367fb60d1e4728ae1d449d8e21a
Allisa LeBeuf <al5906@gmail.com>
2018-10-23 14:05:50 -0700

    code added, table populating

--- Lab11_First_MVC_App/Controllers/HomeController.cs
+++ Lab11_First_MVC_App/Controllers/HomeController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Lab11_First_MVC_App.Models;
 
 namespace Lab11_First_MVC_App.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public ViewResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int firstyear, int secondyear)
+        {
+            return RedirectToAction("Results", new { firstyear, secondyear });
+        }
+
+        public IActionResult Results(int firstyear, int secondyear)
+        {
+            var people = TimePerson.GetPersons(firstyear, secondyear);
+            return View(people);
         }
 
     }
